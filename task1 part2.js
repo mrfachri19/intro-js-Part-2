2bc4656605ce0f461845c7b6c561ba33d8a68692
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-13 01:23:15 +0700

    Add files via upload

---
+++ task1 part2.js
@@ -0,0 +1,19 @@
+// charAt menampilkan karakter ke sekian | dimulai dari indekx ke 0
+let name = 'fachri';
+let nama2 = 'maulana';
+console.log(name.charAt(5));
+
+//toLocaleLowerCase()	
+console.log(name.toUpperCase());	
+
+//toLowerCase()
+console.log(name.toLowerCase());
+
+//slice()
+console.log(name.slice());
+
+//	concat()
+console.log(name.concat(nama2));
+
+//includes()
+console.log(name.includes(nama2));
