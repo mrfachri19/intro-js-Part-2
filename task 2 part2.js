ea25869cf6749f4a3286b8595f8edae7743665a6
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-13 10:38:16 +0700

    Update task 2 part2.js

--- task 2 part2.js
+++ task 2 part2.js
@@ -1,19 +1,23 @@
+//calback : function yang dikirimkan sebagai parameter function lain
 const name = [
     "Abigail","Alexandra", "Alison", "Amanda",
     "Angela", "Bella", "Carol", "Caroline",
     "Carolyn", "Deirdre", "Diana", "Elizabeth", 
     "Ella","Faith","Olivia","Penelope",
   ];
-  
-  const cariNama = (key, limit, callback) => {
+  // fungsi carinama
+  const cariNama = (keyword, limit, callback) => {
+    // mencari array
     const cariArr = name.filter((nama) => {
-      return nama.toLowerCase().includes(key);
+      return nama.toLowerCase().includes(keyword);
     });
-  
+  // menentukan limit
+  //['alexandra', 'amanda', 'angela', 'diana']
     const limitNama = cariArr.slice(0, limit);
     callback(limitNama);
   };
-  
+
+  // result
   const result = (nama) => {
     console.log(nama);
   };
