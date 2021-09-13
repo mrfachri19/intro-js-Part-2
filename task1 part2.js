02c8494ff60c6c7eca6520f0d60317f3b032ec56
fachri <83835862+mrfachri19@users.noreply.github.com>
2021-09-13 09:07:50 +0700

    Update task1 part2.js

--- task1 part2.js
+++ task1 part2.js
@@ -1,19 +1,53 @@
 // charAt menampilkan karakter ke sekian | dimulai dari indekx ke 0
 let name = 'fachri';
-let nama2 = 'maulana';
+let arr =['sandi','andi'];
+
 console.log(name.charAt(5));
 
-//toLocaleLowerCase()	
-console.log(name.toUpperCase());	
+// push
+//menambah elemen di akhir array
+arr.push('dody');
+console.log(arr);
 
-//toLowerCase()
-console.log(name.toLowerCase());
+//pop
+//elemen terakirnya dihapus
+arr.pop();
+console.log(arr);
 
-//slice()
-console.log(name.slice());
+//toLowerCase() 
+// mengubah menjadi huruf kecil
+console.log('ALPHABET'.toLowerCase()); // 'alphabet'
 
-//	concat()
-console.log(name.concat(nama2));
+// .toUpperCase()
+// mengubah menjadi huruf besar
+console.log('alphabet'.toUpperCase()); // 'ALPHABET'
 
-//includes()
-console.log(name.includes(nama2));
+//unshift
+// menambah elemen awal
+arr.unshift('dody');
+console.log(arr);
+
+//shift
+// menghilangkan awal elemen
+arr.shift();
+console.log(arr);
+
+
+//splice 
+//menyisipkan array ditengah
+//splice(index awal,mau dihapus berapa, elemen baru1, elemen baru2, ... )
+arr.splice(0,0, 'dodi','doni');
+console.log(arr);
+
+//slice
+// slice(awal terbawa, akhir tidak)
+let arr2 = arr.slice(1,3);
+console.log(arr2);
+
+//filter 
+// mencari banyak nilai
+let angka = [1,2,3,4,5,6,7];
+let angka2 = angka.filter(function(x){
+    return x >2;
+});
+console.log(angka2);
